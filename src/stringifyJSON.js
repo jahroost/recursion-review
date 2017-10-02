564480593cb68f83d8ca4649fb08cf232ecfa72e
jahroost <claysmart@gmail.com>
2017-10-02 15:59:09 -0500

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj + '';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === undefined || typeof obj === 'function') {
+    return;
+  }
+
+  var intermediate = [];
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      intermediate.push(stringifyJSON(obj[i]));
+    }
+    return '[' + intermediate.toString() + ']';
+  }
+
+  for (var key in obj) {
+    if (obj[key] !== undefined && typeof obj[key] !== 'function' && typeof key !== 'function') {
+      intermediate.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+  }
+  // return '{' + intermediate + '}';
+
+  return '{' + intermediate.toString() + '}';
+
 };
